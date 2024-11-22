254e8cc52abf6bf3b078794e14109bfc0b0e3d9f
imnb57 <imnitesh56@gmail.com>
2024-11-22 14:16:03 +0545

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  output:'export',
-  basePath:'/app1'
+  output: "export",
+  basePath: isGitHubPages ? "/app1" : "", // Add `/app1` only for GitHub Pages
+  trailingSlash: true, // Required for static exports
 };
 
 export default nextConfig;
